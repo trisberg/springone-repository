58f17139f1e3a054d25274c13cdc51572931f41b
Thomas Risberg <trisberg@vmware.com>
2011-10-21 21:15:22 -0400

    Adding the repository implementation of the Mongo Bookshelf app

---
+++ src/main/java/org/springframework/data/demo/domain/Book.java
@@ -0,0 +1,71 @@
+package org.springframework.data.demo.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Book {
+	
+	private String title;
+	
+	@DBRef
+	private Author author;
+	
+	@Id
+	private String isbn;
+	
+	private BigDecimal price;
+	
+	private Date published;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Date getPublished() {
+		return published;
+	}
+
+	public void setPublished(Date published) {
+		this.published = published;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn
+				+ ", price=" + price + ", published=" + published + "]";
+	}
+
+}
